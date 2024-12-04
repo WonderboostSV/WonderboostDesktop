40938dd0646fc63d87fa7295bba8b5fbabde91a2
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2024-12-03 20:48:48 -0600

    Corrección de conexión entre electron y react

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import dotenv from 'dotenv';
+
+// Cargar variables del archivo .env
+dotenv.config();
 
 // Obtener la ruta de __dirname en módulos ES
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -11,18 +15,34 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.resolve(__dirname, 'preload.js'),
       contextIsolation: true,
       enableRemoteModule: false,
       nodeIntegration: false,
     },
   });
 
-  // Carga tu aplicación React (modo desarrollo o producción)
+  // Obtén la URL desde el archivo .env
   const startURL = process.env.ELECTRON_START_URL || path.join(__dirname, '../dist/index.html');
   mainWindow.loadURL(startURL);
+  console.log('Cargando URL:', startURL);
 
   mainWindow.on('closed', () => (mainWindow = null));
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools(); // Solo abre DevTools en desarrollo
+  }
+  mainWindow.webContents.on('console-message', (event, level, message, line, sourceId) => {
+    if (
+      message.includes("Unknown VE context") ||
+      message.includes("Request Autofill")
+    ) {
+      // Ignorar mensajes específicos
+      return;
+    }
+    console.log(`Console [${level}]: ${message} (line ${line}, source ${sourceId})`);
+  });
+
 };
 
 // Evento para iniciar la aplicación
