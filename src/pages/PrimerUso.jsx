1cf33c8673abb95b40fe5d63c77a5e3d4da916bf
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2024-12-05 02:26:54 -0600

    Diseño de plantilla de login y de primer uso

--- src/pages/PrimerUso.jsx
+++ src/pages/PrimerUso.jsx
@@ -1,9 +1,158 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const PrimerUso = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
     return (
         <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-            <h1 className="text-4xl text-gray-800 dark:text-gray-200">
-                Primer uso
-            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-5xl shadow-lg rounded-lg overflow-hidden">
+                {/* Sección izquierda con imagen */}
+                <div className="hidden md:block bg-yellow-500">
+                    <img
+                        src="/src/assets/leftSignUp.avif"
+                        alt="Registro"
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+
+                {/* Sección derecha con el formulario */}
+                <div className="bg-white dark:bg-gray-800 p-8 flex flex-col justify-center">
+                    <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-6">
+                        CREA TU CUENTA AHORA
+                    </h2>
+                    <form className="space-y-4">
+                        {/* Campo Usuario */}
+                        <div>
+                            <label
+                                htmlFor="username"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                            >
+                                Usuario:
+                            </label>
+                            <input
+                                type="text"
+                                id="username"
+                                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                                placeholder="Tu usuario"
+                            />
+                        </div>
+
+                        {/* Campo Correo */}
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                            >
+                                Correo:
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                                placeholder="Tu correo"
+                            />
+                        </div>
+
+                        {/* Campo Contraseña */}
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                            >
+                                Contraseña:
+                            </label>
+                            <div className="relative">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                                    placeholder="Tu contraseña"
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute inset-y-0 right-3 flex items-center"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    {showPassword ? (
+                                        <FaEyeSlash className="text-gray-500 dark:text-gray-400" />
+                                    ) : (
+                                        <FaEye className="text-gray-500 dark:text-gray-400" />
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+
+                        {/* Campo Repetir Contraseña */}
+                        <div>
+                            <label
+                                htmlFor="confirm-password"
+                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                            >
+                                Repite la clave:
+                            </label>
+                            <div className="relative">
+                                <input
+                                    type={showConfirmPassword ? "text" : "password"}
+                                    id="confirm-password"
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                                    placeholder="Repite tu contraseña"
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute inset-y-0 right-3 flex items-center"
+                                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                >
+                                    {showConfirmPassword ? (
+                                        <FaEyeSlash className="text-gray-500 dark:text-gray-400" />
+                                    ) : (
+                                        <FaEye className="text-gray-500 dark:text-gray-400" />
+                                    )}
+                                </button>
+                            </div>
+                        </div>
+
+                        {/* Generar clave segura */}
+                        <div className="flex items-center">
+                            <input
+                                type="checkbox"
+                                id="generate-password"
+                                className="w-4 h-4 text-yellow-500 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-400 dark:bg-gray-700 dark:border-gray-600"
+                            />
+                            <label
+                                htmlFor="generate-password"
+                                className="ml-2 text-sm text-gray-700 dark:text-gray-300"
+                            >
+                                Generar clave segura
+                            </label>
+                        </div>
+
+                        {/* Aceptar términos */}
+                        <div className="flex items-center">
+                            <input
+                                type="checkbox"
+                                id="terms"
+                                className="w-4 h-4 text-yellow-500 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-400 dark:bg-gray-700 dark:border-gray-600"
+                            />
+                            <label
+                                htmlFor="terms"
+                                className="ml-2 text-sm text-gray-700 dark:text-gray-300"
+                            >
+                                Acepto los términos y condiciones de uso.
+                            </label>
+                        </div>
+
+                        {/* Botón de registro */}
+                        <button
+                            type="submit"
+                            className="w-full bg-yellow-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-yellow-400 dark:hover:bg-yellow-500"
+                        >
+                            Registrarse
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,79 +1,96 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
     return (
-        <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Wonderboost"
-                        src="/src/assets/logo.avif"
-                        className="mx-auto h-10 w-auto"
-                    />
-                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                        Inicia sesión
-                    </h2>
-                </div>
+        <div className="h-screen flex bg-gray-100 dark:bg-gray-900">
+            {/* Sección izquierda con la imagen */}
+            <div className="hidden md:flex w-1/2 bg-cover bg-center" style={{ backgroundImage: `url('/src/assets/leftLogin.avif')` }}>
+            </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
-                        <div>
-                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                                Email
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    autoComplete="email"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                />
-                            </div>
-                        </div>
+            {/* Sección derecha del formulario */}
+            <div className="w-full md:w-1/2 flex flex-col items-center justify-center px-8">
+                {/* Logo */}
+                <img
+                    alt="Wonderboost"
+                    src="/src/assets/logo.avif"
+                    className="mb-6 h-12 w-auto"
+                />
 
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                                    Clave
-                                </label>
-                                <div className="text-sm">
-                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                        ¿Recuperar clave?
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    autoComplete="current-password"
-                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                />
-                            </div>
-                        </div>
+                {/* Título */}
+                <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-4">
+                    INICIO DE SESIÓN
+                </h1>
+                <p className="text-gray-600 dark:text-gray-400 mb-8">
+                    Bienvenido a tu cuenta nuevamente
+                </p>
 
-                        <div>
+                {/* Formulario */}
+                <form className="w-full max-w-sm space-y-4">
+                    {/* Campo de usuario */}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            Usuario:
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Ingresa tu usuario"
+                            id="email"
+                            className="w-full px-4 py-2 mt-1 bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:ring-blue-500 focus:border-blue-500 focus:outline-none text-gray-900 dark:text-gray-200"
+                        />
+                    </div>
+
+                    {/* Campo de contraseña */}
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300">
+                            Clave de acceso:
+                        </label>
+                        <div className="relative">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                placeholder="Ingresa tu clave"
+                                className="w-full px-4 py-2 mt-1 bg-gray-200 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md focus:ring-blue-500 focus:border-blue-500 focus:outline-none text-gray-900 dark:text-gray-200"
+                            />
                             <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                type="button"
+                                className="absolute inset-y-0 right-3 flex items-center text-gray-500 dark:text-gray-400 focus:outline-none"
+                                onClick={() => setShowPassword(!showPassword)}
                             >
-                                Accede
+                                {showPassword ? (
+                                    <FaEyeSlash className="text-gray-500 dark:text-gray-400" />
+                                ) : (
+                                    <FaEye className="text-gray-500 dark:text-gray-400" />
+                                )}
                             </button>
                         </div>
-                    </form>
+                    </div>
 
-                    <p className="mt-10 text-center text-sm/6 text-gray-500">
-                        ¿No tienes cuenta?{' '}
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            ¡Registrate aquí!
+                    {/* Botón de iniciar sesión */}
+                    <button
+                        type="submit"
+                        className="w-full py-2 bg-yellow-500 hover:bg-yellow-600 text-white font-medium rounded-md transition duration-300"
+                    >
+                        INICIAR SESIÓN
+                    </button>
+                </form>
+
+                {/* Opciones adicionales */}
+                <div className="mt-4 text-sm text-gray-600 dark:text-gray-400 text-center">
+                    <a href="#" className="hover:underline">
+                        ¿Olvidó su clave?
+                    </a>
+                    <p className="mt-2">
+                        ¿No tienes una cuenta?{' '}
+                        <a href="#" className="text-blue-500 hover:underline">
+                            ¡Crea hoy mismo tu cuenta!
                         </a>
                     </p>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 };
 
 export default Login;
