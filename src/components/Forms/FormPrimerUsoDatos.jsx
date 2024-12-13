d3fb2f0deb88c000ed0c7d70f38fa9ac44b478a0
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2024-12-13 01:34:45 -0600

    Diseño de plantillas de primer uso

---
+++ src/components/Forms/FormPrimerUsoDatos.jsx
@@ -0,0 +1,136 @@
+const FormRegistroDatosPersonales = () => {
+    return (
+        <div className="bg-white dark:bg-gray-800 p-8 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-6">
+                REGISTRA TUS DATOS
+            </h2>
+            <form className="space-y-4">
+                {/* Campo Nombres */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                        <label
+                            htmlFor="nombres"
+                            className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        >
+                            Nombres:
+                        </label>
+                        <input
+                            type="text"
+                            id="nombres"
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                            placeholder="Tus nombres"
+                        />
+                    </div>
+
+                    {/* Campo Apellidos */}
+                    <div>
+                        <label
+                            htmlFor="apellidos"
+                            className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                        >
+                            Apellidos:
+                        </label>
+                        <input
+                            type="text"
+                            id="apellidos"
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                            placeholder="Tus apellidos"
+                        />
+                    </div>
+                </div>
+
+                {/* Campo Teléfono */}
+                <div>
+                    <label
+                        htmlFor="telefono"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                    >
+                        Teléfono:
+                    </label>
+                    <input
+                        type="tel"
+                        id="telefono"
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                        placeholder="Tu teléfono"
+                    />
+                </div>
+
+                {/* Campo DUI */}
+                <div>
+                    <label
+                        htmlFor="dui"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                    >
+                        DUI:
+                    </label>
+                    <input
+                        type="text"
+                        id="dui"
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                        placeholder="Tu DUI"
+                    />
+                </div>
+
+                {/* Campo Dirección */}
+                <div>
+                    <label
+                        htmlFor="direccion"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                    >
+                        Dirección:
+                    </label>
+                    <input
+                        type="text"
+                        id="direccion"
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                        placeholder="Tu dirección"
+                    />
+                </div>
+
+                {/* Campo Fecha de Nacimiento */}
+                <div>
+                    <label
+                        htmlFor="fecha-nacimiento"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                    >
+                        Fecha de nacimiento:
+                    </label>
+                    <input
+                        type="date"
+                        id="fecha-nacimiento"
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
+                    />
+                </div>
+
+                {/* Campo Foto */}
+                <div>
+                    <label
+                        htmlFor="foto"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+                    >
+                        Foto:
+                    </label>
+                    <div className="flex items-center">
+                        <label
+                            htmlFor="upload-photo"
+                            className="cursor-pointer flex items-center justify-center w-20 h-20 bg-gray-200 dark:bg-gray-700 rounded-full border border-gray-300 dark:border-gray-600"
+                        >
+                            <span className="text-gray-500 dark:text-gray-400">+</span>
+                        </label>
+                        <input type="file" id="upload-photo" className="hidden" />
+                    </div>
+                </div>
+
+                {/* Botón de registro */}
+                <button
+                    type="submit"
+                    className="w-full bg-slate-900 dark:bg-yellow-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-slate-950 dark:hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                >
+                    Registrarse
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default FormRegistroDatosPersonales;
--- src/pages/PrimerUso.jsx
+++ src/pages/PrimerUso.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import FormRegistroCuentaAdministrador from "../components/Forms/FormPrimerUsoUsuario";
+import FormRegistroDatosPersonales from "../components/Forms/FormPrimerUsoDatos";
 
 const PrimerUso = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [formStep, setFormStep] = useState(1); // 1 para registro de cuenta, 2 para datos personales
+
+    const handleNextStep = () => {
+        setFormStep(2); // Cambiar al siguiente formulario
+    };
 
     return (
         <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
@@ -16,141 +20,13 @@ const PrimerUso = () => {
                         className="h-full w-full object-cover"
                     />
                 </div>
-
                 {/* Sección derecha con el formulario */}
-                <div className="bg-white dark:bg-gray-800 p-8 flex flex-col justify-center">
-                    <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200 mb-6">
-                        CREA TU CUENTA AHORA
-                    </h2>
-                    <form className="space-y-4">
-                        {/* Campo Usuario */}
-                        <div>
-                            <label
-                                htmlFor="username"
-                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-                            >
-                                Usuario:
-                            </label>
-                            <input
-                                type="text"
-                                id="username"
-                                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                                placeholder="Tu usuario"
-                            />
-                        </div>
-
-                        {/* Campo Correo */}
-                        <div>
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-                            >
-                                Correo:
-                            </label>
-                            <input
-                                type="email"
-                                id="email"
-                                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                                placeholder="Tu correo"
-                            />
-                        </div>
-
-                        {/* Campo Contraseña */}
-                        <div>
-                            <label
-                                htmlFor="password"
-                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-                            >
-                                Contraseña:
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    id="password"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                                    placeholder="Tu contraseña"
-                                />
-                                <button
-                                    type="button"
-                                    className="absolute inset-y-0 right-3 flex items-center"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? (
-                                        <FaEyeSlash className="text-gray-500 dark:text-gray-400" />
-                                    ) : (
-                                        <FaEye className="text-gray-500 dark:text-gray-400" />
-                                    )}
-                                </button>
-                            </div>
-                        </div>
-
-                        {/* Campo Repetir Contraseña */}
-                        <div>
-                            <label
-                                htmlFor="confirm-password"
-                                className="block text-sm font-medium text-gray-700 dark:text-gray-300"
-                            >
-                                Repite la clave:
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={showConfirmPassword ? "text" : "password"}
-                                    id="confirm-password"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200"
-                                    placeholder="Repite tu contraseña"
-                                />
-                                <button
-                                    type="button"
-                                    className="absolute inset-y-0 right-3 flex items-center"
-                                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                                >
-                                    {showConfirmPassword ? (
-                                        <FaEyeSlash className="text-gray-500 dark:text-gray-400" />
-                                    ) : (
-                                        <FaEye className="text-gray-500 dark:text-gray-400" />
-                                    )}
-                                </button>
-                            </div>
-                        </div>
-
-                        {/* Generar clave segura */}
-                        <div className="flex items-center">
-                            <input
-                                type="checkbox"
-                                id="generate-password"
-                                className="w-4 h-4 text-yellow-500 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-400 dark:bg-gray-700 dark:border-gray-600"
-                            />
-                            <label
-                                htmlFor="generate-password"
-                                className="ml-2 text-sm text-gray-700 dark:text-gray-300"
-                            >
-                                Generar clave segura
-                            </label>
-                        </div>
-
-                        {/* Aceptar términos */}
-                        <div className="flex items-center">
-                            <input
-                                type="checkbox"
-                                id="terms"
-                                className="w-4 h-4 text-yellow-500 border-gray-300 rounded focus:ring-yellow-500 dark:focus:ring-yellow-400 dark:bg-gray-700 dark:border-gray-600"
-                            />
-                            <label
-                                htmlFor="terms"
-                                className="ml-2 text-sm text-gray-700 dark:text-gray-300"
-                            >
-                                Acepto los términos y condiciones de uso.
-                            </label>
-                        </div>
-
-                        {/* Botón de registro */}
-                        <button
-                            type="submit"
-                            className="w-full bg-yellow-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-yellow-400 dark:hover:bg-yellow-500"
-                        >
-                            Registrarse
-                        </button>
-                    </form>
+                <div>
+                    {formStep === 1 ? (
+                        <FormRegistroCuentaAdministrador onNext={handleNextStep} />
+                    ) : (
+                        <FormRegistroDatosPersonales />
+                    )}
                 </div>
             </div>
         </div>
